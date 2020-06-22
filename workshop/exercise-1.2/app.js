eb802225c2b2213e2d1bc419166753aaca14545b
Antonio Placido <aplacido1992@gmail.com>
2020-06-22 06:01:17 -0400

    Question 1.2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,22 @@
-// Exercise 1.2
-// ------------
-console.log('exercise 1.2');
+// 
+
+const body = document.querySelector(`body`);
+const result = document.getElementById(`result`);
+const theTimer = Math.round(Math.random()*5);
+console.log(theTimer);
+const countdown = setTimeout(gameover,(theTimer*1000));
+
+document.getElementById(`time`).innerText = theTimer;
+
+function gameover(){
+    result.innerText = `GAME OVER`;
+    body.removeEventListener(`click`,clicky);
+}
+
+function clicky (){
+    result.innerText = `BIG WINNER`;
+    body.removeEventListener(`click`,clicky); 
+    clearTimeout(countdown);
+}
+
+body.addEventListener(`click`,clicky);
