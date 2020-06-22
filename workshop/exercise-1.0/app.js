89b17c4d04c8b1e888ac50d3e7da3f9cc37adc8f
Antonio Placido <aplacido1992@gmail.com>
2020-06-22 04:28:00 -0400

    Question 1 - Remember to target IDs

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,60 @@
 // Exercise 1.0
 // ------------
-console.log('exercise-1');
+
+// function add(first, second){
+//     console.log(first+second);
+// }
+
+// add(2,3);
+// add(4,5);
+
+
+
+// // const body = document.querySelector(`body`);
+// // body.addEventListener(`click`, function (){
+// //     console.log('exercise-1');
+// //     body.removeEventListener('click',function (){
+// //         console.log('exercise-1' );
+// // });
+
+
+// /*target the "body" */
+
+// const body = document.querySelector(`body`);
+// const msg = document.getElementsByClassName(`msg`);
+
+// function clickyEvent(){
+//     msg.innerText = `Exercise 1`;
+//     body.removeEventListener(`click`, clicky);
+// }
+
+// body.addEventListener(`click`,clickyEvent);
+//     // msg.innertext = `Exercise 1`;
+//     // body.removeEventListener(`click`);  //**need to call on the whole function  */
+
+//     const body = document.querySelector('body');
+//     const result = document.getElementById('result');
+    
+
+
+// // Add the event listener
+// body.addEventListener('click', clickEvent);
+
+
+// Create variables with DOM items
+const body = document.querySelector('body');
+const result = document.getElementById('main');            
+
+// The 'click' function
+function clicky() {
+  result.innerText = 'Exercise 1';
+  body.removeEventListener('click', clicky);
+}
+
+body.addEventListener('click', clicky);
+
+// document.getElementsByClassName(`main`).addEventListener(`click`, showMsg);
+
+// function showMsg(){
+//     document.getElementById(`main`).innerText = `exercise 1`
+// }
