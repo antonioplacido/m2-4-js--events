dafdd346dd52caeb029dc05d0f7e91a96a8ef7bd
Antonio Placido <aplacido1992@gmail.com>
2020-07-12 23:07:38 -0400

    2.2

--- workshop/exercise-2.2/app.js
+++ workshop/exercise-2.2/app.js
@@ -1,24 +1,18 @@
 const container = document.querySelector("body");
 const bCounter = [];
 
-let clicked = false;
-
 function clicky(event) {
-  console.log(clicked);
-  if (clicked === false) {
+  if (event.target.style.backgroundColor === "green") {
     event.target.style.backgroundColor = "red";
     clicked = true;
-    console.log(clicked);
   } else {
     event.target.style.backgroundColor = "green";
-    clicked = false;
-    console.log(clicked);
   }
   return clicked;
 }
 
 function removelisteners() {
-    bCounter.forEach(function (button) {
+  bCounter.forEach(function (button) {
     button.removeEventListener("mousedown", clicky);
   });
 }
@@ -34,8 +28,7 @@ for (i = 1; i <= 20; i++) {
   bCounter.push(button);
 }
 
-
-const stylesheet = document.createElement('link');
-stylesheet.rel = 'stylesheet';
-stylesheet.href = 'styles.css';
+const stylesheet = document.createElement("link");
+stylesheet.rel = "stylesheet";
+stylesheet.href = "styles.css";
 document.head.appendChild(stylesheet);
